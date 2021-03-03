65deab0a462b19d911cac98fdaff7f41d2ff04a8
Eu <eulier1@gmail.com>
2021-03-03 09:41:35 -0300

    add recurssion

---
+++ Algorithm/Recursion/factorial.js
@@ -0,0 +1,23 @@
+// Write two functions that finds the factorial of any number. One should use recursive, the other should just use a for loop
+
+function findFactorialRecursive(number) {
+  if (number === 0 || number === 1) {
+      return 1
+  }
+  return number * findFactorialRecursive(number -1)
+}
+  
+function findFactorialIterative(number) {
+  let answer = 1;
+  if (number === 2) {
+    answer === 2
+  }
+
+  for (let i = 2; i <= number; i++) {
+    answer = answer * i;
+    
+  }
+  return answer;
+}
+  
+console.log(findFactorialRecursive(2))
---
+++ Algorithm/Recursion/reverse-string.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    if (str === "") {
+        return ""
+    } else {
+        return reverseString( str.substring(1) ) + str.charAt(0)
+    }
+}
+
+console.log(reverseString('yoyo mastery'))
