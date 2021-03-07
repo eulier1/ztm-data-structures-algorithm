b79d428a54916636a5193ebd7f0391f6de8ff439
Eu <eulier1@gmail.com>
2021-03-07 13:12:08 -0300

    selection sort

---
+++ Algorithm/Sorting/selection.js
@@ -0,0 +1,18 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function selectionSort(array) { // O(n2)
+    const length = array.length
+    for (let i = 0; i < length; i++) {
+        let minIndex = i
+        let temp = array[i]
+        for (let j = i+1; j < length; j++) {
+            if ( array[j] < array[minIndex]) {
+                minIndex = j
+            }
+        }
+        array[i] = array[minIndex]
+        array[minIndex] = temp
+    }
+}
+selectionSort(numbers)
+console.log(numbers)
